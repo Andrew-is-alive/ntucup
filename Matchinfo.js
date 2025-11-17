64b812aee4b5949045682eb1f1a527a72e0879b7
Andrew-is-alive <83465187+Andrew-is-alive@users.noreply.github.com>
2025-11-18 01:51:51 +0800

    popuplayout

--- Matchinfo.js
+++ Matchinfo.js
@@ -9,18 +9,17 @@ function showMatchinfo(matchDiv, match){
         document.getElementById('popup').innerHTML = `
                     <button id="close-popup" class="close-popup-button" style="position: absolute; top: 8px; right: 8px; font-size: 24px; background: transparent; border: none; cursor: pointer;">&times;</button>
                     <h3 class="popup-title">${match.teamAID} vs ${match.teamBID}</h3>
+                    <h6 class="popup-subtitle">${match.group} ${match.date}</h6>
                     <div class="match-container">
                         <h3 class="popup-title">${match.status && match.teamAID !== match.teamBID? `${match.winner}贏` : ""}</h3>
-                        <div style="display: flex; justify-content: space-between; margin: 0 10px;">
-                            <label class="group-label" style="font-weight: bold;">Group: ${match.group}</label>
-                            <label class="group-label" style="font-weight: bold;">Date: ${match.date}</label>
-                        </div>
                         <div style="display: flex; flex-direction: column; gap: 10px; margin-top: 10px;">
                             <div class="set-container">
                                 <label>Set 1:</label>
                                 <input type="text" maxlength="2" class="score-input" data-set="set1" data-team="0" value="${match.set1[0] || ''}">
                                 <span>:</span>
                                 <input type="text" maxlength="2" class="score-input" data-set="set1" data-team="1" value="${match.set1[1] || ''}">
+                            </div>
+                            <div class="set-container">
                                 <label>Set 2:</label>
                                 <input type="text" maxlength="2" class="score-input" data-set="set2" data-team="0" value="${match.set2[0] || ''}">
                                 <span>:</span>
@@ -32,6 +31,8 @@ function showMatchinfo(matchDiv, match){
                                 <input type="text" maxlength="2" class="score-input" data-set="set3" data-team="0" value="${match.set3[0] || ''}">
                                 <span>:</span>
                                 <input type="text" maxlength="2" class="score-input" data-set="set3" data-team="1" value="${match.set3[1] || ''}">
+                            </div>
+                            <div class="set-container">
                                 <label>Official: </label>
                                 <div class="official-dropdown">
                                     <input type="text" class="official-search" value="${match.official || ''}" data-field="official" placeholder="Search official...">
